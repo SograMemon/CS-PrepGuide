496f508f525911df8c2d68f8cdea783db967f6cd
Unknown <sogra.memon@dal.ca>
2018-02-10 20:25:33 -0400

    Filter changes

---
+++ app/src/main/java/ca/dal/cs/web/cs_prepguide/jobListing.java
@@ -0,0 +1,13 @@
+package ca.dal.cs.web.cs_prepguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class jobListing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_listing);
+    }
+}
