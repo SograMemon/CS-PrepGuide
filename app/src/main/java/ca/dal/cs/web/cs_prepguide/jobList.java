56a78d0770fe0463961ce2958209820f10b29035
FARAAZ DHUKA <fr894333@dal.ca>
2018-04-04 16:54:43 -0300

    Image/Logo of each Company added
    -used firebase to add company logo

--- app/src/main/java/ca/dal/cs/web/cs_prepguide/jobList.java
+++ app/src/main/java/ca/dal/cs/web/cs_prepguide/jobList.java
@@ -1,6 +1,7 @@
 package ca.dal.cs.web.cs_prepguide;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,12 +9,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 import static ca.dal.cs.web.cs_prepguide.R.*;
 import static ca.dal.cs.web.cs_prepguide.R.layout.list_layout;
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 /**
  * Created by SM on 3/5/18.
@@ -50,11 +56,23 @@ public class jobList extends ArrayAdapter<job> {
             TextView textViewCompany = (TextView) listViewItem.findViewById(id.txt_Company);
             TextView textViewMatchSkill = (TextView) listViewItem.findViewById(id.txt_match);
 
+            ImageView logo = listViewItem.findViewById(R.id.logo);
+
             textViewCompany.setText(job1.getJobCompany());
             textViewStream.setText(job1.getJobStream());
             textViewTitle.setText(job1.getJobTitle());
             textViewType.setText(job1.getJobType());
             textViewMatchSkill.setText(job1.getJobFilter());
+
+            if (job1.getJobCompany().equals("Google"))
+            Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/cs-prepguide.appspot.com/o/companyLogo%2FCompnayLogosSquarGoogle.jpg?alt=media&token=a4b631b1-f975-4493-aeef-a5c5525c26a7").fit().centerCrop().into(logo);
+
+            if (job1.getJobCompany().equals("Facebook"))
+                Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/cs-prepguide.appspot.com/o/companyLogo%2FCompnayLogosSquarFB.jpg?alt=media&token=62803779-3eb9-4c4a-a692-358af5febffd").fit().centerCrop().into(logo);
+
+            if (job1.getJobCompany().equals("Microsoft"))
+                Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/cs-prepguide.appspot.com/o/companyLogo%2Fif_Microsoft_New_Logo_98785.png?alt=media&token=13feaac8-b909-4e49-992f-8c8d616bac1b").fit().centerCrop().into(logo);
+
         }
 
         return listViewItem;
